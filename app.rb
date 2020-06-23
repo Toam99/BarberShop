a928851afd92b090b1a3007327f7ba5e3846c420
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-06-23 18:35:21 +0800

    Save visit iinfo to database

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 configure do
-  @db = SQLite3::Database.new 'barbershop.db'
-  @db.execute 'CREATE TABLE IF NOT EXISTS  
+    db = get_db
+    db.execute 'CREATE TABLE IF NOT EXISTS  
         "Users" 
         (
             "Id" INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -53,9 +53,22 @@ post '/visit' do
         return erb :visit
     end
 
+    db = get_db
+    db.execute 'insert into 
+    Users 
+    (
+        username, 
+        phone, 
+        date_stamp, 
+        barber, 
+        #color
+    )
+    values ( ?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color] 
+
     erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
 
-# add 
-# require 'sqlite3'
+def get_db 
+    return SQLite3::Database.new 'barbershop.db'
+end
